64cf4caad365824e37d694bfe0e1c4df091fde8b
“Prachi-QT” <prachi.ghatole@quore-tex.com>
2026-05-11 13:10:59 +0530

    added student controller

--- src/main/java/com/shravanee/crud_practice/service/StudentService.java
+++ src/main/java/com/shravanee/crud_practice/service/StudentService.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class StudentService
+public clasStudentService
 {
     @Autowired
     private StudentRepository repo;
